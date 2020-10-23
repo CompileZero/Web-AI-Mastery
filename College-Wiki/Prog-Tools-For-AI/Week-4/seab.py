427149d1819f6133cff5172aaf6e16836936b253
Atharva Kulkarni <atharvaak66@gmail.com>
2020-10-23 12:59:41 +0100

    added all assignment

---
+++ College-Wiki/Prog-Tools-For-AI/Week-4/seab.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+
+dots = sns.load_dataset("dots")
+sns.relplot(
+    data=dots, kind="line",
+    x="time", y="firing_rate", col="align",
+    hue="choice", size="coherence", style="choice",
+    facet_kws=dict(sharex=False),
+)
